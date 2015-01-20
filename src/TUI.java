93152b96265498b46afc3d2987d6bce5f28412c5
EdwinFl <e.t.flapper@student.utwente.nl>
2015-01-20 11:53:13 +0100

    update

--- src/TUI.java
+++ src/TUI.java
@@ -82,8 +82,8 @@ public class TUI implements View{
 	public void printBoard(Observable o) {
 		Board board = ((Game) o).getBoard();
 		Mark[][] field = board.getField();
-		for(int i=board.HEIGHT-1;i>=0;i--) {
-    		for(int j=0;j<board.WIDTH;j++) {
+		for(int i=0;i<Board.HEIGHT;i--) {
+    		for(int j=0;j<Board.WIDTH;j++) {
         		System.out.print(" \t|" + field[i][j] + " \t|");
         	}
     		System.out.println();
